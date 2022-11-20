1cd387bbf9dda0ac74b9b4ee2e67b406435a7738
kimathinjoki <dfkimathinjoki@gmail.com>
2022-11-20 16:19:16 +0300

    to be completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+const element = document.getElementById("main");
+
+element.remove()
+
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+
+
+newHeader.id = "victory";
+
+
+
+newHeader.textContent="KIMATHI is the champion";
+
+
+// const words = document.getElementById("victory")
+// words.innerHTML= <h1><strong>KIMATHI</strong> is a champion</h1>
