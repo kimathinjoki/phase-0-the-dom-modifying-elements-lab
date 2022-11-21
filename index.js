d710479359ab02a11885124130887cfb2f1f36e1
kimathinjoki <dfkimathinjoki@gmail.com>
2022-11-21 08:00:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ element.remove()
 
 
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
+// document.body.append(newHeader);
+
+newHeader.setAttribute("id","victory");
+
+// newHeader= "victory";
 
 
-
-newHeader.id = "victory";
-
-// newHeader.setAttribute =("id","victory");
-
-
-newHeader.textContent="KIMATHI is the champion";
-
+const me = "Kimathi";
+newHeader.textContent= `${me} is the champion`;
 
 // const words = document.getElementById("victory")
 // words.innerHTML= <h1><strong>KIMATHI</strong> is a champion</h1>
